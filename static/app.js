80ff7098c25b341a447686cadc4b0c9adf645731
angelruizmorales <93920185+angelruizmorales@users.noreply.github.com>
2022-12-19 18:50:45 +0100

    todo

---
+++ static/app.js
@@ -0,0 +1,13 @@
+// Ángel_Ruiz_Morales
+//
+// Completar JS aqui
+function cambiopantalla(){
+    document.getElementById('primera').classList.add('noview');
+    document.getElementById('segunda').classList.remove('noview');
+}
+
+function alerta(){
+    document.getElementById('primera').classList.add('noview');
+    document.getElementById('segunda').classList.remove('noview');
+    alert("Datos guardados correctamente")
+}
