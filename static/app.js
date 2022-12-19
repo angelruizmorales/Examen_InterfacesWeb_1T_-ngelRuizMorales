2235a35a12efef269af222b6e363b713099757ee
angelruizmorales <93920185+angelruizmorales@users.noreply.github.com>
2022-12-19 18:46:09 +0100

    delete

--- static/app.js
+++
@@ -1,7 +0,0 @@
-// Ángel_Ruiz_Morales
-//
-// Completar JS aqui
-function cambiopantalla(){
-    document.getElementById('primera').classList.add('noview');
-    document.getElementById('segunda').classList.remove('noview');
-}
